fa6db9f9e5596eb4d66f34422efcb025f1b41882
Zach <zwc101@gmail.com>
2019-07-16 08:43:51 -0700

    adding reddit scraper; doesn't work yet

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,7 +1,29 @@
-const world = "Cat";
+import Parser from "rss-parser";
+import fetch from "node-fetch"; 
+import { resolve } from "dns";
+import { parse } from "node-html-parser";
 
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
+const uri = "https://www.reddit.com/r/news.rss"
+
+
+export async function readFeed() {
+  const parser = new Parser();
+  let feed = await parser.parseURL(uri);
+  console.log(feed.title);
+
+    feed.items!.forEach(item => {
+      fetch(item.link)
+        .then(body => {
+          body.text().then(txt =>
+            {
+              console.log(parse(txt).childNodes.toString());
+            });
+        });
+  })
 }
 
-console.log(hello());
+// export async function visit(uri: string | undefined) {
+//   return fetch(uri);
+// }
+
+readFeed();
