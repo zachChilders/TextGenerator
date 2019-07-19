014d5ebf5071ff7c0c26b20b392ceefd4de7bdf6
Zach <zwc101@gmail.com>
2019-07-18 20:09:01 -0700

    scraper works

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -4,9 +4,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import fetch from "node-fetch";
 
-const minimumArticleLength = process.env.SCRAPER_MIN_ARTICLE_LENGTH || 500;
-const uri = process.env.SCRAPER_SUBREDDIT || "https://www.reddit.com/r/news+worldnews+politics/.json";
-const connstring = process.env.SCRAPER_CONNSTRING || "";
+const connstring = process.env.APPSETTING_SCRAPER_CONNSTRING || "";
 const port = 80;
 
 // MongoDB Schema
@@ -25,11 +23,14 @@ export async function handleUrl(url: string): Promise<IArticle> {
       const $ = cheerio.load(html);
       return {url, body: $("p").text()} ; // We should probably be smarter than <p>
     });
-
 }
 
 // Read a reddit feed
 export async function readFeed() {
+  // Refresh App settings
+  const uri = process.env.APPSETTING_SCRAPER_SUBREDDIT || "https://www.reddit.com/r/news+worldnews+politics/.json";
+  const minimumArticleLength = Number(process.env.APPSETTING_SCRAPER_MIN_ARTICLE_LENGTH) || 250;
+
   // Setup MongoDB connection
   const client = await MongoClient.connect(connstring, {useNewUrlParser: true});
   const db = client.db("articles");
@@ -52,7 +53,7 @@ export async function readFeed() {
           // New article, scrape it.
           console.log(`Found Article: ${post.data.url}`);
           const article = await handleUrl(post.data.url);
-
+          console.log(article.body.length);
           // Filter out shorter text.
           if (article.body.length > minimumArticleLength) {
             console.log("Adding to DB");
@@ -64,9 +65,9 @@ export async function readFeed() {
     .catch((err) => console.log(err));
 }
 
-console.log(`connstring: ${process.env.SCRAPER_CONNSTRING}`);
+console.log(`connstring: ${connstring}`);
 // Run
-setInterval(() => { readFeed(); }, 2000);
+setInterval(() => { readFeed(); }, 20000);
 
 express()
   .get("/keepalive", (req, res) => res.send(`Alive\n${new Date()}`))
